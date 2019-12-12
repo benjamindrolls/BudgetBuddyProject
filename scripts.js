a270e2f84bd17c37308336375d7e70ac6405afcc
ben <benjamindrolls@gmail.com>
2019-12-11 21:49:55 -0500

    java notes and comments added

---
+++ scripts.js
@@ -0,0 +1,17 @@
+//have a variable for budget and then have it displayed
+
+//researching progress bar, looks like the java will control how much width the bar will take up
+
+//take user input for purchase, category drop down and option description and store them
+//purchase amount stored into an category arrays to be displayed
+//create a class for itemPurchase with constructors of price and description
+//setting price = price and description = ""
+
+//create arrays for all category's and use either a if statement or a switch case to determine what array info will be sorted
+
+//create function to that loops over every arrays and finds dollar amounts, to total them up
+//function paramater would the array
+
+//function(array.bill)
+
+//take data for budget and user input and display dollars spent for each category
