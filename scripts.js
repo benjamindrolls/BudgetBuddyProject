54253d37b20df8aefb9e96fc7f78d2cdb415d5d3
Rachael <rachaelvruggink@gmail.com>
2019-12-15 22:28:01 -0500

    sidebar navigation functioning

--- scripts.js
+++ scripts.js
@@ -42,8 +42,18 @@ function getTransaction(){
     transactionValue = document.getElementById('moneySpent').value;
     items.push(transactionValue);  
     console.log(items);
-   
 }
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "150px";
+    document.getElementById("main").style.marginRight = "150px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+  }
 
 //let test = 0;
 
