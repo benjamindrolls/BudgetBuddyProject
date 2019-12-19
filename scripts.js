8feec4c526f1cd6e4208386948d36f82faabe543
ben <benjamindrolls@gmail.com>
2019-12-18 19:22:16 -0500

    added sams baller javascript like team on da back doe

--- scripts.js
+++ scripts.js
@@ -1,119 +1,76 @@
 "use strict";
-
-class BudgetTracker {
-    constructor(price, description = '') {
+/* Each ItemPurchase object represents a single transaction */
+class ItemPurchase {
+    constructor(price, description = '') {    // if a description isn't provided, default to an empty string
         this.price = price;
         this.description = description;
     }
 }
-
-//have a variable for budget and then have it displayed
-const totalBudget = 100;
+let totalBudget = 0;
 let totalSpent = 0;
-
-//Entertainment
-const category1Budget = 0.1 * totalBudget;
-//Food
-const category2Budget = 0.3 * totalBudget;
-//Clothing
-const category3Budget = 0.1 * totalBudget;
-//Bills
-const category4Budget = 0.5 * totalBudget;
-
-let category1Spent = 0;
-let category2Spent = 0;
-let category3Spent = 0;
-let category4Spent = 0;
-
-//researching progress bar, looks like the java will control how much width the bar will take up
-/**
- * @src for progress bar https://www.w3schools.com/howto/howto_js_progressbar.asp
- */
-//take user input for purchase, category drop down and option description and store them
-//purchase amount stored into an category arrays to be displayed
-//create a class for itemPurchase with constructors of price and description
-//setting price = price and description = ""
-
-const form = document.querySelector('.form');
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    const price = Number(document.querySelector('#moneySpent').value);
-
-    if (price > 0) {
-        totalSpent +=  price;
-        moneyLeft();
-
-        const latestTransaction = new BudgetTracker(price);
-
-        const selectedCategory = document.querySelector('#categoryDropdown').value;
-        let priceDisplay;
-
-        switch (selectedCategory) {
-            case '1':
-                category1Spent += latestTransaction.price;
-
-                priceDisplay = document.querySelector('#dollarsCategory1');
-                priceDisplay.innerHTML = `$${category1Spent.toFixed(2)} / $${category1Budget.toFixed(2)}`;
-
-                break;
-            case '2':
-                category2Spent += latestTransaction.price;
-
-                priceDisplay = document.querySelector('#dollarsCategory2');
-                priceDisplay.innerHTML = `$${category2Spent.toFixed(2)} / $${category2Budget.toFixed(2)}`;
-
-                break;
-            case '3':
-                category3Spent += latestTransaction.price;
-
-
-                priceDisplay = document.querySelector('#dollarsCategory3');
-                priceDisplay.innerHTML = `$${category3Spent.toFixed(2)} / $${category3Budget.toFixed(2)}`;
-
-                break;
-            case '4':
-                category4Spent += latestTransaction.price;
-
-                priceDisplay = document.querySelector('#dollarsCategory4');
-                priceDisplay.innerHTML = `$${category4Spent.toFixed(2)} / $${category4Budget.toFixed(2)}`;
-
-                break;
-        }
-    }
-});
-
+let categoryBudget = [0, 0, 0, 0];
+let categorySpent = [0, 0, 0, 0];
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "150px";
     document.getElementById("main").style.marginRight = "150px";
-  }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
+}
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginRight = "0";
-  }
-  function weeklyBudget() {
-    const userBudget = document.getElementById("userBudget").value;
-    document.getElementById('budgetDisplay').innerHTML = `$${userBudget}`;
-    return userBudget;
-  }
-function moneyLeft() {
-  const remainingPercent = totalSpent / weeklyBudget() * 100;
-  console.log(remainingPercent);
-  document.getElementById("myBar").style.width = `${remainingPercent}%`;
 }
-
-
-//create arrays for all category's and use either a if statement or a switch case to determine what array info will be sorted
-
-//create function to that loops over every arrays and finds dollar amounts, to total them up
-//function paramter would the array
-
-//function(array.bill)
-
-//look into spread operator
-
-//take data for budget and user input and display dollars spent for each category
+/* Sets the weekly budget to the number that the user enters, then sets category budgets based on that */
+function weeklyBudget() {
+    totalBudget = document.getElementById("userBudget").value;
+    document.getElementById('budgetDisplay').innerHTML = `$${totalBudget}`;
+    categoryBudget = [0.1 * totalBudget,    // Entertainment
+        0.3 * totalBudget,                  // Food
+        0.1 * totalBudget,                  // Clothing
+        0.5 * totalBudget];                 // Bills
+    for (let i = 0; i < categoryBudget.length; i++) {
+        updateCategory(i);                  // update each category line on the page
+    }
+}
+const form = document.querySelector('.form');   // this is the "Enter a transaction" form
+/* When the user clicks Submit, process the transaction */
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const price = Number(document.querySelector('#moneySpent').value);
+    if (price > 0) {
+        // TODO: Make sure this transaction doesn't go over total budget
+        totalSpent +=  price;   // update total budget
+        moneyLeft();            // update progress bar
+        const latestTransaction = new ItemPurchase(price);
+        // find correct array index for categoryBudget/categorySpent based on dropdown box
+        const selectedCategory = Number(document.querySelector('#categoryDropdown').value) - 1;
+        updateCategory(selectedCategory, latestTransaction.price);
+    }
+});
+/* updates the amount spent in the proper category, then displays it on the page */
+function updateCategory(category, price = 0) {
+    categorySpent[category] += price;
+    let priceDisplay;       // priceDisplay is which line of category spending to update in the HTML
+    switch (category) {
+        case 0:
+            priceDisplay = document.querySelector('#dollarsCategory1');
+            break;
+        case 1:
+            priceDisplay = document.querySelector('#dollarsCategory2');
+            break;
+        case 2:
+            priceDisplay = document.querySelector('#dollarsCategory3');
+            break;
+        case 3:
+            priceDisplay = document.querySelector('#dollarsCategory4');
+            break;
+    }
+    // update the HTML line, using toFixed(2) to put it in money format
+    priceDisplay.innerHTML = `$${categorySpent[category].toFixed(2)} / $${categoryBudget[category].toFixed(2)}`;
+}
+/* Calculates what percentage of total budget has been spent and updates the progress bar */
+function moneyLeft() {
+    const remainingPercent = totalSpent / totalBudget * 100;
+    console.log(remainingPercent);
+    document.getElementById("myBar").style.width = `${remainingPercent}%`;
+}
