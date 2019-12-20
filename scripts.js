40cb1c6764d19612833355b65ef98b8a095a45b2
ben <benjamindrolls@gmail.com>
2019-12-19 21:07:43 -0500

    added welcome message, and functionality that makes the user enter their weekly budget first

--- scripts.js
+++ scripts.js
@@ -33,6 +33,7 @@ function weeklyBudget() {
     for (let i = 0; i < categoryBudget.length; i++) {
         updateCategory(i);                  // update each category line on the page
     }
+    document.getElementById("moneySpent").disabled = false;
 }
 
 function decreaseTotal() {
@@ -92,7 +93,7 @@ function moneyLeft() {
     }
 } 
 
-
+//plays cow sounds and displays cash cow telling you that you are spending to much
 let moo = new Audio("./sounds/Cow.mp3")
 let heard = new Audio("./sounds/SmallHerd.mp3")
 function overBudget(){
@@ -105,4 +106,35 @@ function overBudget(){
         heard.play();
     }
 }
+//disables transaction text box so the user has to enter weekly income first
+function enterBudget() {
+    if (totalBudget > 0) {
+        return true;
+    } else {
+        alert("enter a weekly budget before continuing.")
+        document.getElementById("moneySpent").disabled = true;
+    }
+}
 
+
+// Get the modal
+let modal = document.getElementById("welcome");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+function getModal () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
