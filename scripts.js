8a952fb0bc9b5c7eecc406b3c3337603ae41e2b3
jch2617 <jch2617@gmail.com>
2019-12-22 16:37:10 -0500

    new branch who dis

--- scripts.js
+++ scripts.js
@@ -1,25 +1,18 @@
 "use strict";
+
 /* Each ItemPurchase object represents a single transaction */
 class ItemPurchase {
-    constructor(price, description = '') {    // if a description isn't provided, default to an empty string
+    constructor(price, category, description = '') {    // if a description isn't provided, default to an empty string
         this.price = price;
-        // this.description = description;
-    } 
+        this.category = category;
+        this.description = description;
+    }
 }
-let totalBudget;
+
+let transactionHistory = [];
+
+let totalBudget = 0;
 let totalSpent = 0;
-// const totalBudget = 100;
-
-let category1Spent = 0;
-let category2Spent = 0;
-let category3Spent = 0;
-let category4Spent = 0;
-
-function weeklyIncome() {
-    let userBudget = document.getElementById("userBudget").value;
-    document.getElementById('budgetDisplay').innerHTML = `$${userBudget}`;
-    return userBudget;
-} 
 
 let categoryBudget = [0, 0, 0, 0];
 let categorySpent = [0, 0, 0, 0];
@@ -29,11 +22,13 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "150px";
     document.getElementById("main").style.marginRight = "150px";
 }
+
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginRight = "0";
 }
+
 /* Sets the weekly budget to the number that the user enters, then sets category budgets based on that */
 function weeklyBudget() {
     totalBudget = document.getElementById("userBudget").value;
@@ -42,39 +37,52 @@ function weeklyBudget() {
         0.3 * totalBudget,                  // Food
         0.1 * totalBudget,                  // Clothing
         0.5 * totalBudget];                 // Bills
-    } for (let i = 0; i < categoryBudget.length; i++) {
+    for (let i = 0; i < categoryBudget.length; i++) {
         updateCategory(i);                  // update each category line on the page
+    }
+    document.getElementById("moneySpent").disabled = false;
 }
 
+/* Updates the total amount available to spend on the webpage */
 function decreaseTotal() {
    const newtotal =  totalBudget - totalSpent;
    document.getElementById('budgetDisplay').innerHTML = `$${newtotal}`;
 } 
 
 const form = document.querySelector('.form');   // this is the "Enter a transaction" form
+
 /* When the user clicks Submit, process the transaction */
 form.addEventListener('submit', e => {
     e.preventDefault();
+
     const price = Number(document.querySelector('#moneySpent').value);
+
     if (price > 0) {
-        // TODO: Make sure this transaction doesn't go over total budget
         totalSpent +=  price;   // update total budget
-        moneyLeft();            // update progress bar
+        updateProgressBar();    // update progress bar
         decreaseTotal();        // decreases transactions from total on top center of page
-        const latestTransaction = new ItemPurchase(price);
+        overBudget();           // display cow if at or over total budget
+
+        const selectedCategory = document.querySelector('#categoryDropdown');
+        const categoryName = selectedCategory.options[selectedCategory.selectedIndex].text;
+        
+        const latestTransaction = new ItemPurchase(price, categoryName);
+        transactionHistory.unshift(latestTransaction);      // add latest transaction to the start of array
+
         // find correct array index for categoryBudget/categorySpent based on dropdown box
-        const selectedCategory = Number(document.querySelector('#categoryDropdown').value) - 1;
-        updateCategory(selectedCategory, latestTransaction.price);
+        const selectedIndex = selectedCategory.selectedIndex;
+        updateCategory(selectedIndex, latestTransaction.price);
+
+        updateHistory();        // update transaction history box
     }
-    overBudget();
 });
-
-
 
 /* updates the amount spent in the proper category, then displays it on the page */
 function updateCategory(category, price = 0) {
     categorySpent[category] += price;
+
     let priceDisplay;       // priceDisplay is which line of category spending to update in the HTML
+
     switch (category) {
         case 0:
             priceDisplay = document.querySelector('#dollarsCategory1');
@@ -89,13 +97,26 @@ function updateCategory(category, price = 0) {
             priceDisplay = document.querySelector('#dollarsCategory4');
             break;
     }
+
     // update the HTML line, using toFixed(2) to put it in money format
     priceDisplay.innerHTML = `$${categorySpent[category].toFixed(2)} / $${categoryBudget[category].toFixed(2)}`;
 }
 
+/* Updates the content of the "Your Recent Transactions" modal box */
+function updateHistory() {
+    const categoryHTML = document.createElement('p');
+    categoryHTML.innerText = transactionHistory[0].category;
+    let column = document.querySelector('#transaction-category');
+    column.insertBefore(categoryHTML, column.firstChild);
+
+    const priceHTML = document.createElement('p');
+    priceHTML.innerText = `$${transactionHistory[0].price.toFixed(2)}`;
+    column = document.querySelector('#transaction-price');
+    column.insertBefore(priceHTML, column.firstChild);
+}
 
 /* Calculates what percentage of total budget has been spent and updates the progress bar */
-function moneyLeft() {
+function updateProgressBar() {
     const remainingPercent = totalSpent / totalBudget * 100;
     document.getElementById("myBar").style.width = `${remainingPercent}%`;
     if (totalBudget < totalSpent) {
@@ -103,17 +124,71 @@ function moneyLeft() {
     }
 } 
 
-
-let moo = new Audio("./sounds/Cow.mp3")
-let heard = new Audio("./sounds/SmallHerd.mp3")
+/* Alerts the user if their spending reaches or exceeds the total budget */
 function overBudget(){
+    //plays cow sounds and displays cash cow telling you that you are spending to much
+    const moo = new Audio("./sounds/Cow.mp3")
+    const heard = new Audio("./sounds/SmallHerd.mp3")
+
     if (totalSpent == totalBudget){
         document.getElementById("cashCow").style.display = "contents";
         moo.play();
-}else if (totalSpent > totalBudget){
+    } else if (totalSpent > totalBudget){
         document.getElementById("alertCow").style.display = "contents";
         document.getElementById("cashCow").style.display = "none";
         heard.play();
+    } else {
+        document.getElementById("alertCow").style.display = "none";
+        document.getElementById("cashCow").style.display = "none";
+    }
+}
+//disables transaction text box so the user has to enter weekly income first
+function enterBudget() {
+    if (totalBudget > 0) {
+        return true;
+    } else {
+        alert("Please enter a weekly budget before continuing.")
+        document.getElementById("moneySpent").disabled = true;
     }
 }
 
+
+// Get the modal
+let modal = document.getElementById("welcome");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+function getModal () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+////////////////////////////////////////////////////////
+// Get the modal
+let purchaseModal = document.getElementById("transactionList");
+
+let btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+let spanPan = document.getElementsByClassName("close")[1];
+
+btn.onclick = function() {
+  purchaseModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanPan.onclick = function() {
+  purchaseModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of either modal, close it
+window.onclick = function(event) {
+    if (event.target == modal || event.target == purchaseModal) {
+      modal.style.display = "none";
+      purchaseModal.style.display = "none";
+    }
+  }
